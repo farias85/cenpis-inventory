1cd34a5c63741d137c8d534e8d685b6c3dc08a4c
Felipe Rdguez Arias <farias@uo.edu.cu>
2017-03-03 04:46:28 -0500

    activo fijo empezando los reportes

--- inv/src/java/cu/cenpis/gps/inv/controller/ActivoFijoController.java
+++ inv/src/java/cu/cenpis/gps/inv/controller/ActivoFijoController.java
@@ -2,6 +2,8 @@ package cu.cenpis.gps.inv.controller;
 
 import cu.cenpis.gps.inv.data.service.ActivoFijoService;
 import cu.cenpis.gps.inv.data.entity.ActivoFijo;
+import cu.cenpis.gps.inv.data.entity.Apunte;
+import cu.cenpis.gps.inv.data.service.ApunteService;
 import cu.cenpis.gps.inv.util.Bundle;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -18,16 +20,18 @@ public class ActivoFijoController extends BaseController<ActivoFijo, java.lang.L
         this.activoFijoService = activoFijoService;
     }
 
-    public ActivoFijoController() {
-        super(ActivoFijo.class);
+    private ApunteService apunteService;
+
+    public ApunteService getApunteService() {
+        return apunteService;
     }
 
-    @Override
-    public List<ActivoFijo> getItems() {
-        if (items == null) {
-            items = getFacade().findNamedQuery("ActivoFijo.findByRevisionActiva");
-        }
-        return items;
+    public void setApunteService(ApunteService apunteService) {
+        this.apunteService = apunteService;
+    }
+
+    public ActivoFijoController() {
+        super(ActivoFijo.class);
     }
 
     @PostConstruct
@@ -50,5 +54,28 @@ public class ActivoFijoController extends BaseController<ActivoFijo, java.lang.L
     public void destroy() {
         super.destroy(Bundle.getString("ActivoFijoDeleted"));
     }
-}
 
+    @Override
+    public List<ActivoFijo> getItems() {
+        if (items == null) {
+            items = getFacade().findNamedQuery("ActivoFijo.findByRevisionActiva");
+        }
+        return items;
+    }
+
+    public List<ActivoFijo> getOtrosActivos() {
+        return getFacade().findNamedQuery("ActivoFijo.findByRotulo", "rotulo", selected.getRotulo());
+    }
+
+    public List<Apunte> getActivoApuntes() {
+        return apunteService.findNamedQuery("Apunte.findByRotulo", "rotulo", selected.getRotulo());
+    }
+
+    public List<ActivoFijo> getActivosNuevos() {
+        return getFacade().findNamedQuery("ActivoFijo.findByRotulo", "rotulo", "011620");
+    }
+    
+    public List<ActivoFijo> getActivosYaNoEstan() {
+        return getFacade().findNamedQuery("ActivoFijo.findByRotulo", "rotulo", "011620");
+    }
+}
--- inv/src/java/cu/cenpis/gps/inv/controller/ApunteController.java
+++ inv/src/java/cu/cenpis/gps/inv/controller/ApunteController.java
@@ -2,7 +2,10 @@ package cu.cenpis.gps.inv.controller;
 
 import cu.cenpis.gps.inv.data.service.ApunteService;
 import cu.cenpis.gps.inv.data.entity.Apunte;
+import cu.cenpis.gps.inv.data.service.UsuarioService;
 import cu.cenpis.gps.inv.util.Bundle;
+import cu.cenpis.gps.inv.util.JsfUtil;
+import java.util.Date;
 import javax.annotation.PostConstruct;
 
 public class ApunteController extends BaseController<Apunte, java.lang.Integer> {
@@ -15,6 +18,16 @@ public class ApunteController extends BaseController<Apunte, java.lang.Integer> 
 
     public void setApunteService(ApunteService apunteService) {
         this.apunteService = apunteService;
+    }
+
+    private UsuarioService usuarioService;
+
+    public UsuarioService getUsuarioService() {
+        return usuarioService;
+    }
+
+    public void setUsuarioService(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
     }
 
     public ApunteController() {
@@ -41,5 +54,29 @@ public class ApunteController extends BaseController<Apunte, java.lang.Integer> 
     public void destroy() {
         super.destroy(Bundle.getString("ApunteDeleted"));
     }
-}
 
+    @Override
+    protected void setEmbeddableKeys() {
+        super.setEmbeddableKeys(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    protected void initializeEmbeddableKey() {
+        if (selected.getUsuario() == null) {
+            UsuarioController usuarioController = JsfUtil.getController(UsuarioController.class);
+            if (usuarioController.getActiveUser() != null) {
+                selected.setUsuario(usuarioController.getActiveUser());
+            } else {
+                selected.setUsuario(usuarioService.find(1L));
+            }
+        }
+        if (selected.getFecha() == null) {
+            selected.setFecha(new Date());
+        }
+        if (selected.getRotulo() == null || selected.getRotulo().isEmpty()) {
+            ActivoFijoController activoFijoController = JsfUtil.getController(ActivoFijoController.class);
+            selected.setRotulo(activoFijoController.getSelected().getRotulo());
+        }
+    }
+
+}
